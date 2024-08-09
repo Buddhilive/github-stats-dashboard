706ead1a94694c8e1b2215b73be843f08c6da70f
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-08-09 16:53:23 +0530

    Fetch repo languages

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,11 +6,24 @@ import { GithubStatsService } from '../shared/services/github-stats.service';
   standalone: true,
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
   private githubStatsService = inject(GithubStatsService);
-  fetchUserInfo(){
-    this.githubStatsService.getUserRepos('buddhilive').subscribe(data => console.log(data));
+  fetchUserInfo() {
+    this.githubStatsService.getUserRepos('buddhilive').subscribe((data: any) => {
+      console.log(data);
+      const languageData: any = [];
+      data.map((repo: any) => {
+        this.githubStatsService
+          .getRepoLanguagesStats(repo.languages_url)
+          .subscribe((langs: any) => {
+            languageData.push({ repo: repo.name, languages: langs });
+          },
+          (error) => console.log(error),
+          () => console.log(languageData)
+        );
+      });
+    });
   }
 }
--- src/app/shared/services/github-stats.service.ts
+++ src/app/shared/services/github-stats.service.ts
@@ -7,15 +7,19 @@ import { inject, Injectable } from '@angular/core';
 export class GithubStatsService {
   private HTTP_CLIENT = inject(HttpClient);
   private BASE_URL = 'https://api.github.com/';
+  private headers = new HttpHeaders({
+    Authorization: 'token ' + 'ghp_7jJw5j2sHnOqXs6WqK0Qs7p0iA8xjO9vOJ6v'
+  });
 
   getUserInfo(username: string) {
     return this.HTTP_CLIENT.get(this.BASE_URL + 'users/' + username);
   }
 
   getUserRepos(username: string) {
-    const headers = new HttpHeaders({
-      Authorization: 'token ' + 'ghp_7jJw5j2sHnOqXs6WqK0Qs7p0iA8xjO9vOJ6v'
-    });
-    return this.HTTP_CLIENT.get(this.BASE_URL + 'users/' + username + '/repos?per_page=100');
+    return this.HTTP_CLIENT.get(this.BASE_URL + 'users/' + username + '/repos?per_page=100', { headers: this.headers });
+  }
+
+  getRepoLanguagesStats(languagesUrl: string) {
+    return this.HTTP_CLIENT.get(languagesUrl, { headers: this.headers });
   }
 }
