05f7016b7529c6b1c27a59f72c294b032215d310
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-08-11 09:43:01 +0530

    Implement language stats component

--- src/app/components/language-stats/language-stats.component.ts
+++ src/app/components/language-stats/language-stats.component.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, Input, OnInit } from '@angular/core';
+import { GithubStatsService } from '../../shared/services/github-stats.service';
+import { catchError, forkJoin } from 'rxjs';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-language-stats',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ChartModule],
   templateUrl: './language-stats.component.html',
   styleUrl: './language-stats.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LanguageStatsComponent {}
+export class LanguageStatsComponent implements OnInit {
+  @Input() repoData!: Array<any>;
+  private githubStatsService = inject(GithubStatsService);
+  chartData: any;
+
+  ngOnInit(): void {
+    this.fetchLanguageStats();
+  }
+
+  fetchLanguageStats() {
+    this.repoData = JSON.parse(localStorage.getItem('github-stats-repos')!);
+    const languageStatsObservables = this.repoData.map((repo: any) =>
+      this.githubStatsService.getRepoLanguageStats(repo.languages_url).pipe(
+        catchError((error) => {
+          console.error(`Failed to fetch language stats for repo: ${repo.name}`, error);
+          return [];
+        })
+      )
+    );
+
+    forkJoin(languageStatsObservables).subscribe(
+      (repoLanguageData: Array<any>) => {
+        localStorage.setItem('github-stats-repos-lan', JSON.stringify(repoLanguageData));
+        const allLangSummary = this.sumByKeyAndTotal(repoLanguageData);
+        localStorage.setItem('github-stats-lan-summary', JSON.stringify(allLangSummary));
+      },
+      (error) => console.error('An error occurred while fetching language stats', error)
+    );
+  }
+
+  private sumByKeyAndTotal(data: Array<any>) {
+    const groupedData: Object = data.reduce((acc, curr) => {
+      Object.entries(curr).forEach(([key, value]) => {
+        acc[key] = (acc[key] || 0) + value;
+      });
+      return acc;
+    }, {});
+
+    const totalSum = Object.values(groupedData).reduce((acc, val) => acc + val, 0);
+    this.chartData = this.formatDataForChart(groupedData);
+
+    return { groupedData, totalSum };
+  }
+
+  private formatDataForChart(groupedData: Object) {
+    const labels = Object.keys(groupedData);
+    const data = Object.values(groupedData);
+
+    return {
+      labels,
+      datasets: [{
+        label: 'Data',
+        data,
+        backgroundColor: Array.from({ length: labels.length }, () => `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`)
+      }]
+    };
+  }
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,17 +2,16 @@ import { Component, inject } from '@angular/core';
 import { GithubStatsService } from '../shared/services/github-stats.service';
 import { FormsModule } from '@angular/forms';
 import { GithubUserProfile } from '../shared/constants/github-user.interface';
-import { catchError, forkJoin } from 'rxjs';
-import { ChartModule } from 'primeng/chart';
 import { UserDetailsComponent } from '../components/user-details/user-details.component';
+import { LanguageStatsComponent } from '../components/language-stats/language-stats.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
     FormsModule,
-    ChartModule,
-    UserDetailsComponent
+    UserDetailsComponent,
+    LanguageStatsComponent
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
@@ -23,7 +22,6 @@ export class DashboardComponent {
   username!: string;
   userProfile!: GithubUserProfile;
   repoData: Array<any> = [];
-  chartData: any;
 
   fetchUserInfo() {
     this.githubStatsService.getUserInfo(this.username).subscribe((data: any) => {
@@ -49,57 +47,6 @@ export class DashboardComponent {
     this.githubStatsService.getAllRepos(this.username).subscribe((data: any) => {
       this.repoData = data;
       localStorage.setItem('github-stats-repos', JSON.stringify(data));
-      this.fetchLanguageStats();
     });
   }
-
-  fetchLanguageStats() {
-    this.repoData = JSON.parse(localStorage.getItem('github-stats-repos')!);
-    const languageStatsObservables = this.repoData.map((repo: any) =>
-      this.githubStatsService.getRepoLanguageStats(repo.languages_url).pipe(
-        catchError((error) => {
-          console.error(`Failed to fetch language stats for repo: ${repo.name}`, error);
-          return [];
-        })
-      )
-    );
-
-    forkJoin(languageStatsObservables).subscribe(
-      (repoLanguageData: Array<any>) => {
-        localStorage.setItem('github-stats-repos-lan', JSON.stringify(repoLanguageData));
-        const allLangSummary = this.sumByKeyAndTotal(repoLanguageData);
-        localStorage.setItem('github-stats-lan-summary', JSON.stringify(allLangSummary));
-      },
-      (error) => console.error('An error occurred while fetching language stats', error)
-    );
-  }
-
-  private sumByKeyAndTotal(data: Array<any>) {
-    const groupedData: Object = data.reduce((acc, curr) => {
-      Object.entries(curr).forEach(([key, value]) => {
-        acc[key] = (acc[key] || 0) + value;
-      });
-      return acc;
-    }, {});
-
-    const totalSum = Object.values(groupedData).reduce((acc, val) => acc + val, 0);
-    this.chartData = this.formatDataForChart(groupedData);
-
-    return { groupedData, totalSum };
-  }
-
-  private formatDataForChart(groupedData: Object) {
-    const labels = Object.keys(groupedData);
-    const data = Object.values(groupedData);
-
-    return {
-      labels,
-      datasets: [{
-        label: 'Data',
-        data,
-        backgroundColor: Array.from({ length: labels.length }, () => `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`)
-      }]
-    };
-  }
-
 }
