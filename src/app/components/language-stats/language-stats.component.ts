86b8178bbe792a9354597fef72181e3647872046
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-08-11 08:16:33 +0530

    Add language stats component

---
+++ src/app/components/language-stats/language-stats.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-language-stats',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './language-stats.component.html',
+  styleUrl: './language-stats.component.scss',
+})
+export class LanguageStatsComponent {}
