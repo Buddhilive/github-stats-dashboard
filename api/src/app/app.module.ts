5df94635f8e9e7ff822aa9dbd4f725671bb8e4c7
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-08-12 08:21:41 +0530

    vercel update

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { AppService } from './app.service';
       isGlobal: true
     }),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, './', 'client/browser'),
+      rootPath: join(__dirname, '..', 'client/browser'),
       exclude: ['api/*']
     })
   ],
