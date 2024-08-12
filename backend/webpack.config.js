98e10db73379430c624f50f561e022adc65e92c1
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-08-12 09:44:00 +0530

    Rearrange project

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../api/prod'),
+    path: join(__dirname, '../api'),
   },
   plugins: [
     new NxAppWebpackPlugin({
