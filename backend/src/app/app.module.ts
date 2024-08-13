d8510485e0c2c3747a142f5f29c782a42db44fdb
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-08-13 22:00:00 +0530

    Remove unwanted imports

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,10 +11,6 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    /* ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client/browser'),
-      exclude: ['api/*']
-    }) */
   ],
   controllers: [AppController],
   providers: [AppService],
